ecb1000d324e3f1f65fe8882255de2c0502e1658
Fabio Andrés Triana Rodriguez <124170936+Fabiotriana07@users.noreply.github.com>
2025-11-12 11:59:30 -0500

    mails

--- PlazaNetNegocio/obj/Debug/net9.0/PlazaNetNegocio.AssemblyInfo.cs
+++ PlazaNetNegocio/obj/Debug/net9.0/PlazaNetNegocio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlazaNetNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c8975699e8438df2086a1924a65937942b7102")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2bd3d77722b46f3dc3f28b20b84379255e9a452")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlazaNetNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlazaNetNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
